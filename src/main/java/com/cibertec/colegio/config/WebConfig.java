f46a46d3720d111d73b99b8e8959b942d39035ed
Brian <yamir.coen7@gmail.com>
2026-06-27 15:37:07 -0500

    agregando ruta del fronten

--- src/main/java/com/cibertec/colegio/config/WebConfig.java
+++ src/main/java/com/cibertec/colegio/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
+                        .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*", "https://colegio-frontend.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Content-Disposition");
